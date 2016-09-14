1771b03b2b7b4d6f88c1f034185eaba5f1d3cd72
Teddy Segal <tedward61194@gmail.com>
2016-09-14 10:36:00 -0400

    HW1 Complete

---
+++ HW1.cpp
@@ -0,0 +1,30 @@
+/*
+ * HW1 for Programming languages and Paradigms
+ * Intentionally terrible code showcasing problems with side effects
+ * Due 9/15/16
+ */
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int square(int &i) {
+  i = i*i;
+  return i;
+}
+
+int twice(int &i) {
+  i = i*2;
+  return i;
+}
+
+int once(int &i) {
+  i = i;
+  return i;
+}
+
+main() {
+  int x = 3;
+  int y = square(x) + twice(x) + once(x); //y == 45, not 18
+  cout << y << endl;
+}
